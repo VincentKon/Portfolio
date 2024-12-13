9294bee097a5434fc6772a5953e47cfec2945d04
VincentKon <vincent373kon@gmail.com>
2024-12-14 06:45:56 +0700

    little test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            My Web Development Portfolio
+            My aaWeb Development Portfolio
           </p>
 
           {/* * UI:  Text Generate Effect (https://ui.aceternity.com/components/text-generate-effect) */}
